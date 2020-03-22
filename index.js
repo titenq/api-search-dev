2297342816bbcd99ea1c5ceeef57ffb49f6bcff2
TitenQ <titenq@gmail.com>
2020-03-22 19:49:19 -0300

    express.json() and nodemon

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  return res.send('Hello World!');
+  return res.json({ message: 'Hello World!' });
 });
 
 app.listen(3333, () => console.log('App executando na porta 3333.'));
